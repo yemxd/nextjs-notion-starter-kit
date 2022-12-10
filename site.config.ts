edd19d4d515a848df3ae847af68cd51c1d24f408
yemxd <120251158+yemxd@users.noreply.github.com>
2022-12-10 22:29:32 +0900

    Update site.config.ts
    基本配置改修1

--- site.config.ts
+++ site.config.ts
@@ -2,16 +2,16 @@ import { siteConfig } from './lib/site-config'
 
 export default siteConfig({
   // the site's root Notion page (required)
-  rootNotionPageId: '7875426197cf461698809def95960ebf',
+  rootNotionPageId: '88e6f3fce3f64c89975c6fa7b48f2ddb',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Next.js Notion Starter Kit',
-  domain: 'nextjs-notion-starter-kit.transitivebullsh.it',
-  author: 'Travis Fischer',
+  name: 'MXD樱花宝典',
+  domain: 'https://dune-giraffe-e23.notion.site/88e6f3fce3f64c89975c6fa7b48f2ddb',
+  author: 'Potato Ye',
 
   // open graph metadata (optional)
   description: 'Example Next.js Notion Starter Kit Site',
